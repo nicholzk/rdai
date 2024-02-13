1c7597ea13a978a9fa0108546821c971229773f6
nicholzk <lowzuokai@gmail.com>
2024-02-13 11:08:15 +0800

    working bert integration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+
+from pydantic import BaseModel
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 app = FastAPI()
 
+# Load pre-trained tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased")
+nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
+class InputData(BaseModel):
+    text: str
+
+''''
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -13,3 +25,17 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+'''
+
+
+@app.post("/predict")
+async def predict(data: InputData):
+    # Make prediction using pre-trained model
+    result = nlp(data.text)
+
+    # Return prediction
+    return {"prediction": result[0]['label']}
+
+# LABEL_0 - bad, LABEL_1 - good
+
+    
